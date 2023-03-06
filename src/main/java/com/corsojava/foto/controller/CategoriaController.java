e571601b6cf6462e54a7760e0da3fc488aaa6114
ErosEros <eros.vittori1994@gmail.com>
2023-03-06 02:40:21 +0100

    implement crud categoria + navbar home

--- src/main/java/com/corsojava/foto/controller/CategoriaController.java
+++ src/main/java/com/corsojava/foto/controller/CategoriaController.java
@@ -3,18 +3,19 @@ package com.corsojava.foto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import com.corsojava.foto.model.Categoria;
-import com.corsojava.foto.model.Foto;
+
 import com.corsojava.foto.repo.CategoriaRepository;
 
 import jakarta.validation.Valid;
@@ -33,12 +34,12 @@ public class CategoriaController {
 		Model model) {
 		List<Categoria> categorie; 
 		
-		categorie=categoriaRepo.findAll(Sort.by("name"));
+		categorie=categoriaRepo.findAll();
 		
-		model.addAttribute("ElencoCategorie" , categorie);
-		return "/createCategoria";
+		model.addAttribute("categoria" , categorie);
+		return "/indexCategoria";
 	}
-	
+
 //	METODO CREATE 
 	@GetMapping("/create") // GESTISCO LE RICHIESTE GET /foto/create
 	public String create(Model model) {
@@ -47,23 +48,57 @@ public class CategoriaController {
 		
 		model.addAttribute("categoria" , categoria);
 		
-		return "/editCategoria";
+		return "/createCategoria";
 		
 		
 	}
 	
 //	METODO STORE
-	@PostMapping("/create")  // /categorie/create
+	@PostMapping("/create")  
 	public String store(
 		@Valid @ModelAttribute("categorie") Categoria formCategoria, 
 		BindingResult bindingResult,
 		Model model){
 
 		if (bindingResult.hasErrors())
-			return "createCat";
+			return "createCategoria";
 
 		categoriaRepo.save(formCategoria);
 
 		return "redirect:/categorie"; 
+	}
+	
+//	METODO EDIT 
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, Model model) {
+		
+		Categoria categoria;
+		categoria=categoriaRepo.getReferenceById(id);
+		model.addAttribute("categoria",categoria);
+		return "/editCategoria";
+	}
+	
+//	METODO UPDATE
+	@PostMapping("/edit/{id}")
+	public String update(@Valid @ModelAttribute Categoria formCategoria,
+	BindingResult bindingResult,
+	Model model ) {
+		
+//	VALIDAZIONE
+	if(bindingResult.hasErrors())
+		return "/editCategoria";
+	
+//	Salvo il nuovo oggetto formFoto
+	categoriaRepo.save(formCategoria);
+
+		return "redirect:/categorie";
+	}
+	
+//	METODO DELETE
+	@PostMapping("/delete/{id}") 
+	public String delete(@PathVariable("id") Integer id) {
+		categoriaRepo.deleteById(id);
+		return "redirect:/categorie";
+		
 	}
 }
